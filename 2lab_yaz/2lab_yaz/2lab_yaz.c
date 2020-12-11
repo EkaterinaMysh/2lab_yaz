466fd8a6d1965a9e6903592f4bd707d0272ba6c7
adinterimlat <nastya.ermol31@gmail.com>
2020-12-11 13:01:23 +0300

    final optimization

--- 2lab_yaz/2lab_yaz/2lab_yaz.c
+++ 2lab_yaz/2lab_yaz/2lab_yaz.c
@@ -7,34 +7,6 @@
 int digits[8][10];
 int digits_num[8][10];
 int number_of_sum = 1;
-int already_used = 0;
-
-int if_kolvo(int flag)
-{
-	int i = 0; int j = 1;
-	if (number_of_sum == 2)
-		if ((digits[0][0] != digits[7][0]) && (digits[1][0] != digits[7][0]))
-		{
-			if (already_used != 0)
-			{
-				i = j; j = 0; already_used = 0;
-			}
-			if (flag != 6)
-			{
-				digits_num[i][1] = flag;
-				digits_num[j][1] = 10 - flag;
-				flag++;
-				already_used++;
-			}
-			else {
-				digits_num[i][1] = flag;
-				digits_num[j][1] = flag;
-				flag++;
-			}
-		}
-	return flag;
-}
-
 
 int sravnen() {
 	int k = 2, i1, j1, h, j;
@@ -46,7 +18,6 @@ int sravnen() {
 
 	for (i1 = 0; i1 < number_of_sum; i1++)
 		for (j1 = 1; j1 <= digits_num[i1][0]; j1++)
-		{
 			for (h = 0; h < 8; h++)
 				for (j = 1; j <= digits_num[h][0]; j++)
 				{
@@ -54,7 +25,6 @@ int sravnen() {
 						return k;
 				}
 
-		}
 
 
 	for (j1 = 1; j1 <= digits_num[7][0]; j1++)
@@ -68,8 +38,7 @@ int sravnen() {
 int srav(int a, int slag, int digit)
 {
 	int i, j;
-	for (i = 0; i < 8; i++)
-	{
+	for (i = 0; i < 8; i++) {
 		for (j = 1; j <= digits[i][0]; j++)
 			if (digits[i][j] == digits[slag][digit])
 				digits_num[i][j] = digits_num[slag][digit];
@@ -77,50 +46,93 @@ int srav(int a, int slag, int digit)
 
 }
 
+int evristika()
+{
+	int evr = 0, i, j;
+	if (number_of_sum == 2)
+	{
+		if ((digits_num[0][0] == digits_num[1][0]) && ((digits_num[7][0] - digits_num[0][0]) == 1))
+		{
+			if (digits[0][1] == digits[1][1])
+			{
+				digits_num[0][1] = 5;
+				digits_num[1][1] = 5;
+				int flag = 0;
+				for (i = 0; i < number_of_sum; i++)
+					for (j = 2; j <= digits_num[i][0]; j++)
+					{
+
+						digits_num[i][j] = flag;
+						srav(digits_num[i][j], i, j);
+						flag++;
+					}
+				for (j = 1; j <= digits_num[7][0]; j++)
+					if (j == 1)
+						digits_num[7][j] = 1;
+					else digits_num[7][j] = 0;
+				return evr = 1;
+			}
+		}
+		if (((digits_num[7][0] - digits_num[0][0]) == 1) && ((digits_num[7][0] - digits_num[1][0] == 1)))
+		{
+			if (digits[0][1] == digits[7][1])
+			{
+				digits_num[0][1] = 1;
+				digits_num[7][1] = 1;
+				digits_num[1][1] = 8;
+				return evr = 1;
+			}
+			else if (digits[0][2] == digits[7][1])
+			{
+				digits_num[1][1] = 1;
+				digits_num[7][1] = 1;
+				digits_num[0][1] = 8;
+				return evr = 1;
+			}
+		}
+	}
+
+return evr;
+}
+
 int recurs(int slag, int digit)
 {
-	int res = 1, flag = 0;
+	int res = 1, flag = 0, evr;
 	int i = 1, j;
 	time_t start, end;
 	start = clock();
-	for (i = 0; i < number_of_sum; i++)
-		for (j = 1; j <= digits_num[i][0]; j++)
-		{
-			if (j == 1)
-				digits_num[i][j] = 1;
-			else
-				digits_num[i][j] = flag;
-			srav(digits_num[i][j], i, j);
-			flag++;
-		}
-	for (j = 1; j <= digits_num[7][0]; j++)
-		if (j == 1)
-			digits_num[7][j] = 1;
-		else
-			digits_num[7][j] = 0;
+	evr = evristika();
+	if (evr == 0)
+	{
+		for (i = 0; i < number_of_sum; i++)
+			for (j = 1; j <= digits_num[i][0]; j++)
+			{
+				if (j == 1) digits_num[i][j] = 1;
+				else digits_num[i][j] = flag;
+				srav(digits_num[i][j], i, j);
+				if ((j != 1) && (flag == 0))flag = 2; else if (j != 1) flag++;
+			}
+		for (j = 1; j <= digits_num[7][0]; j++)
+			if (j == 1) digits_num[7][j] = 1;
+			else digits_num[7][j] = 0;
+	}
+
+	flag = 0;
 try_harder:
 	//printmat();
 	{
 		int as = sravnen();
 		if (as == 1) {
-			if (sum() == 0)
-			{
-				printmat();
-				printf("\n!!!!\n");
-				end = clock();
-				printf("\n%f\n", ((double)end - start) / ((double)CLOCKS_PER_SEC));
+			if (sum() == 0) {
+				printmat(); printf("\n!!!!\n"); end = clock(); printf("\n%f\n", ((double)end - start) / ((double)CLOCKS_PER_SEC));
 				exit(0);
 			}
-			else
-			{
-				plus();
-				goto try_harder;
+			else {
+				plus(); goto try_harder;
 			}
 		}
-		else
-		{
-			plus();
-			goto try_harder;
+		else {
+			plus(); goto try_harder;
 		}
 	}
 
@@ -132,82 +144,79 @@ int plus()
 	try :
 		if (digits_num[i][j] != 9)
 		{
-			digits_num[i][j]++;
-			srav(digits_num[i][j], i, j);
+			digits_num[i][j]++; srav(digits_num[i][j], i, j);
 		}
-		else
-		{
-			if (j == 1)
-			{
-				if (i == (number_of_sum - 1))
-					flag = if_kolvo(flag);
-				else
-				{
-					digits_num[i][1] = 1;
-				}
-				srav(digits_num[i][1], i, 1);
-				if (i == 7)
-					i = number_of_sum;
-				i--;
-				j = digits_num[i][0];
-				goto try;
-			}
-			else if (j != 1)
-			{
-				digits_num[i][j] = 0;
-				srav(digits_num[i][1], i, 1);
-				j--;
-				goto try;
-			}
-			else
-			{
-				digits_num[i][j] = 1;
-				srav(digits_num[i][j], i, j);
-				goto try;
+		else {
+			if (j == 1) { if (i == (number_of_sum - 1) && (digits[0][0] != digits[7][0]) && (digits[1][0] != digits[7][0])) flag = if_kolvo(flag); else { digits_num[i][1] = 1; } srav(digits_num[i][1], i, 1); if (i == 7) i = number_of_sum; i--; j = digits_num[i][0]; goto try; }
+			else if (j != 1) { digits_num[i][j] = 0; srav(digits_num[i][1], i, 1); j--; goto try; }
+			else {
+				digits_num[i][j] = 1; srav(digits_num[i][j], i, j); goto try;
 			}
 		}
 
 }
 
+int already_used = 0;
+
+int if_kolvo(int flag)
+{
+	int i = 0; int j = 1;
+	if (number_of_sum == 2) if ((digits[0][0] != digits[7][0]) && (digits[1][0] != digits[7][0]))
+	{
+		if (already_used != 0)
+		{
+			i = j;
+			j = 0; 
+			already_used = 0;
+		}
+		if (flag != 6)
+		{
+			digits_num[i][1] = 10 - flag;
+			digits_num[j][1] = flag;
+			flag++;
+			already_used++;
+		}
+		else 
+		{
+			digits_num[i][1] = flag;
+			digits_num[j][1] = flag;
+			flag++;
+		}
+	}
+	return flag;
+}
 
 int printmat()
 {
 	int i1, j;
-	for (i1 = 0; i1 <= 7; i1++) {
+	for (i1 = 0; i1 < number_of_sum; i1++) {
 		for (j = 1; j <= digits_num[i1][0]; j++)
-			if (digits_num[i1][0] != 0)
-				printf("%i", digits_num[i1][j]);
-		if (i1 == 6)
-			printf("=");
-		else
-			if (i1 == 7)
-				printf("\n");
-			else  if (digits_num[i1][0] != 0 && i1 != (number_of_sum - 1))
-				printf("+");
-
+		{
+			printf("%i", digits_num[i1][j]);
+		}
+		if (i1 != (number_of_sum - 1)) printf("+");
 	}
+	printf("=");
+	for (j = 1; j <= digits_num[7][0]; j++)
+	{
+		printf("%i", digits_num[7][j]);
+	}
+	printf("\n");
 }
-
 
 int sum()
 {
 	int a = 0, b = 0, res_sum = 0, i, j;
-	for (i = 0; i < number_of_sum; i++)
-	{
-		for (j = 1; j <= digits_num[i][0]; j++)
-			a = a * 10 + digits_num[i][j];
+	for (i = 0; i < number_of_sum; i++) {
+		for (j = 1; j <= digits_num[i][0]; j++) a = a * 10 + digits_num[i][j];
 		b += a;
 		a = 0;
 	}
 	a = 0;
-	for (j = 1; j <= digits_num[7][0]; j++)
-		a = a * 10 + digits_num[7][j];
-	if (b == a)
-		res_sum = 0;
-	else res_sum = abs(a - b);
+	for (j = 1; j <= digits_num[7][0]; j++) a = a * 10 + digits_num[7][j];
+	if (b == a) res_sum = 0; else res_sum = abs(a - b);
 	return res_sum;
 }
-
 
 int main()
 {
@@ -223,7 +232,7 @@ int main()
 			digits[j][0] = kolvo; number_of_sum = j + 1; j = 7; fal = i + 1; kolvo = 0;
 		}
 		else if (stroka[i] == '+') {
-			fal = i + 1;  digits[j][0] = kolvo; kolvo = 0; j++;
+			fal = i + 1; digits[j][0] = kolvo; kolvo = 0; j++;
 		}
 		else if (stroka[i] == ' ') fal++;
 		else {
@@ -231,16 +240,9 @@ int main()
 		}
 	}
 	digits[7][0] = kolvo;
-
-
-	for (i = 0; i <= 7; i++) {
-		for (j = 0; j <= digits[i][0]; j++)
-		{
-			if (j == 0) digits_num[i][j] = digits[i][j];
-			else digits_num[i][j] = 10;
-		}
-	}
-
+	memset(digits_num, 0, sizeof(digits_num));
+	for (i = 0; i < number_of_sum; i++) digits_num[i][0] = digits[i][0];
+	digits_num[7][0] = digits[7][0];
 
 	int k = 0, res;
 	recurs(0, 1);
